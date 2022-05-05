2e617c214d7f0eb7ae4cbeb204c2b4e9e2190e05
Kvelail <contactkka@gmail.com>
2022-05-05 20:08:44 +0200

    table body html

--- src/app/components/table/tbody/tbody.component.ts
+++ src/app/components/table/tbody/tbody.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Person } from 'src/app/static-utils-types/types';
+
+import {
+    faCloud,
+    faPaperclip,
+    faFileLines,
+    faEllipsis,
+} from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
     selector: '[app-tbody]',
     templateUrl: './tbody.component.html',
     styleUrls: ['./tbody.component.scss'],
 })
 export class TbodyComponent implements OnInit {
+    persons: Person[] = [
+        {
+            id: 1,
+            name: 'Olivia Smith',
+            img: '/assets/persons-img/Person1.jpeg',
+            phone: '(321) 321-2232',
+            ssn: '333-33-3333',
+            dob: '31/01/83',
+            hired: '02/01/22',
+            cdl: '987654321',
+            cdl_exp: 3,
+            med_exp: 33,
+            mvr_exp: 333,
+            isChecked: false,
+            isDropdownOpened: false,
+        },
+    ];
+
+    faCloud: IconDefinition = faCloud;
+    faPaperclip: IconDefinition = faPaperclip;
+    faFileLines: IconDefinition = faFileLines;
+    faEllipsis: IconDefinition = faEllipsis;
+
     constructor() {}
 
     ngOnInit(): void {}
---
+++ src/app/static-utils-types/types.ts
@@ -0,0 +1,15 @@
+export interface Person {
+    id: number;
+    name: string;
+    img: string;
+    phone: string;
+    ssn: string;
+    dob: string;
+    hired: string;
+    cdl: string;
+    cdl_exp: number;
+    med_exp: number;
+    mvr_exp: number;
+    isChecked: boolean;
+    isDropdownOpened: boolean;
+}
